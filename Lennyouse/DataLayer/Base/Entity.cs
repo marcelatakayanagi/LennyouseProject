ed00972e58798a8da9c9cfff441f1206eac40f14
cmevrgarcia <c.garcia@recodme.pt>
2020-06-22 16:34:31 +0100

    Added Entity and NamedEntity

--- Lennyouse/DataLayer/Base/Entity.cs
+++ Lennyouse/DataLayer/Base/Entity.cs
@@ -1,11 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace DataLayer.Base
 {
-    public class Entity
+    public abstract class Entity
     {
+        [Key]
+        public Guid Id { get; private set; }
 
+        [Display(Name = "Created At")]
+        [Required]
+        public DateTime CreatedAt { get; private set; }
+
+        [Display(Name = "Updated At")]
+        [Required]
+        public DateTime UpdatedAt { get; protected set; }
+
+        private bool _isDeleted;
+        [Display(Name = "Deleted")]
+        [Required]
+        public bool IsDeleted
+        {
+            get => _isDeleted;
+            set
+            {
+                _isDeleted = value;
+                RegisterChange();
+            }
+        }
+
+        protected virtual void RegisterChange()
+        {
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = CreatedAt;
+            _isDeleted = false;
+        }
+
+        protected Entity(Guid id, DateTime createdAt, DateTime updatedAd, bool isDeleted)
+        {
+            Id = id;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAd;
+            _isDeleted = isDeleted;
+        }
     }
 }
---
+++ Lennyouse/DataLayer/Base/NamedEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer.Base
+{
+    public abstract class NamedEntity : Entity
+    {
+        private string _name;
+        [Required]
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                RegisterChange();
+            }
+        }
+
+        protected NamedEntity(string name) : base()
+        {
+            _name = name;
+        }
+
+        protected NamedEntity(Guid id, DateTime createdAt, DateTime updatedAd, bool isDeleted, string name) : base(id, createdAt, updatedAd, isDeleted)
+        {
+            _name = name;
+        }
+    }
+}
