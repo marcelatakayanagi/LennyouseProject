2a628a001d28dc289a86066b74c206ddbe09b364
Samuel Marques <samuel95marques+github@gmail.com>
2020-06-24 18:19:41 +0100

    Added MeasDAO and MenuDAO

---
+++ Lennyouse/DataAccessLayer/DataAccessObjects/MealDataAccessObject.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Recodme.RD.Lennyouse.DataAccessLayer.Contexts;
+using Recodme.RD.Lennyouse.DataLayer.MenuInfo;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recodme.RD.Lennyouse.DataAccessLayer.DataAccessObjects
+{
+    public class MealDataAccessObject
+    {
+        private RestaurantContext _context;
+        public MealDataAccessObject()
+        {
+            _context = new RestaurantContext();
+        }
+
+        #region Create
+        public void Create(Meal meal)
+        {
+            _context.Meals.Add(meal);
+            _context.SaveChanges();
+        }
+
+        public async Task CreateAsync(Meal meal)
+        {
+            await _context.Meals.AddAsync(meal);
+            await _context.SaveChangesAsync();
+        }
+        #endregion
+
+        #region Read
+        public Meal Read(Guid id)
+        {
+            return _context.Meals.FirstOrDefault(x => x.Id == id);
+        }
+        public async Task<Meal> ReadAsync(Guid id)
+        {
+            return await
+                new Task<Meal>
+                (
+                    () => _context.Meals.FirstOrDefault(x => x.Id == id)
+                );
+        }
+        #endregion
+
+        #region Update
+        public void Update(Meal meal)
+        {
+            _context.Entry(meal).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+        public async Task UpdateAsync(Meal meal)
+        {
+            _context.Entry(meal).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+        #endregion
+
+        #region Delete
+        public void Delete(Meal meal)
+        {
+            meal.IsDeleted = true;
+            Update(meal);
+        }
+
+        public void Delete(Guid id)
+        {
+            var item = Read(id);
+            if (item == null) return;
+            Delete(item);
+        }
+
+        public async Task DeleteAsync(Meal meal)
+        {
+            meal.IsDeleted = true;
+            await UpdateAsync(meal);
+        }
+        public async Task DeleteAsync(Guid id)
+        {
+            var item = ReadAsync(id).Result;
+
+            if (item == null) return;
+            await DeleteAsync(item);
+        }
+        #endregion
+    }
+}
---
+++ Lennyouse/DataAccessLayer/DataAccessObjects/MenuDataAccessObject.cs
@@ -0,0 +1,88 @@
+﻿using Recodme.RD.Lennyouse.DataAccessLayer.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Recodme.RD.Lennyouse.DataLayer.MenuInfo;
+
+namespace Recodme.RD.Lennyouse.DataAccessLayer.DataAccessObjects
+{
+    public class MenuDataAccessObject
+    {
+        private RestaurantContext _context;
+        public MenuDataAccessObject()
+        {
+            _context = new RestaurantContext();
+        }
+
+        #region Create
+        public void Create(Menu menu)
+        {
+            _context.Menus.Add(menu);
+            _context.SaveChanges();
+        }
+
+        public async Task CreateAsync(Menu menu)
+        {
+            await _context.Menus.AddAsync(menu);
+            await _context.SaveChangesAsync();
+        }
+        #endregion
+
+        #region Region
+        public Menu Read(Guid id)
+        {
+            return _context.Menus.FirstOrDefault(x => x.Id == id);
+        }
+        public async Task<Menu> ReadAsync(Guid id)
+        {
+            return await
+                new Task<Menu>
+                (
+                    () => _context.Menus.FirstOrDefault(x => x.Id == id)
+                );
+        }
+        #endregion
+
+        #region Update
+        public void Update(Menu menu)
+        {
+            _context.Entry(menu).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+        public async Task UpdateAsync(Menu menu)
+        {
+            _context.Entry(menu).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+        #endregion
+
+        #region Delete
+        public void Delete(Menu menu)
+        {
+            menu.IsDeleted = true;
+            Update(menu);
+        }
+
+        public void Delete(Guid id)
+        {
+            var item = Read(id);
+            if (item == null) return;
+            Delete(item);
+        }
+
+        public async Task DeleteAsync(Menu menu)
+        {
+            menu.IsDeleted = true;
+            await UpdateAsync(menu);
+        }
+        public async Task DeleteAsync(Guid id)
+        {
+            var item = ReadAsync(id).Result;
+
+            if (item == null) return;
+            await DeleteAsync(item);
+        }
+        #endregion
+    }
+}
