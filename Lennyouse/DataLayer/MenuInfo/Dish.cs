ea6938a8f6b6bd90fed46e5673277e047d87e08b
cmevrgarcia <c.garcia@recodme.pt>
2020-07-02 09:53:14 +0100

    Added Course, Dish and Serving Controllers and View Models

--- Lennyouse/DataLayer/MenuInfo/Dish.cs
+++ Lennyouse/DataLayer/MenuInfo/Dish.cs
@@ -7,19 +7,19 @@ namespace Recodme.RD.Lennyouse.DataLayer.MenuInfo
 {
     public class Dish : NamedEntity
     {
-        [ForeignKey("DietaryRestriction")]
+        [ForeignKey("Dietary Restriction")]
         public Guid DietaryRestrictionId { get; set; }
 
         public virtual DietaryRestriction DietaryRestrictions { get; set; }
 
         public virtual ICollection<Serving> Servings { get; set; }
 
-        public Dish(Guid dietaryRestrictionId, string name) : base(name)
+        public Dish(string name, Guid dietaryRestrictionId) : base(name)
         {
             DietaryRestrictionId = dietaryRestrictionId;
         }
 
-        public Dish(Guid id, DateTime createdAt, DateTime updatedAd, bool isDeleted, Guid dietaryRestrictionId, string name) : base(id, createdAt, updatedAd, isDeleted, name)
+        public Dish(Guid id, DateTime createdAt, DateTime updatedAd, bool isDeleted, string name, Guid dietaryRestrictionId) : base(id, createdAt, updatedAd, isDeleted, name)
         {
             DietaryRestrictionId = dietaryRestrictionId;
         }
--- Lennyouse/WebApi/Controllers/MenuInfoControllers/DishController.cs
+++ Lennyouse/WebApi/Controllers/MenuInfoControllers/DishController.cs
@@ -12,56 +12,57 @@ namespace Recodme.RD.Lennyouse.PresentationLayer.WebApi.Controllers.MenuInfoCont
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CourseController : ControllerBase
+    public class DishController : ControllerBase
     {
-        private CourseBusinessObject _bo = new CourseBusinessObject();
+        private DishBusinessObject _bo = new DishBusinessObject();
 
         [HttpPost]
-        public ActionResult Create([FromBody]CourseViewModel vm)
+        public ActionResult Create([FromBody]DishViewModel vm)
         {
-            var c = vm.ToCourse();
-            var res = _bo.Create(c);
+            var d = vm.ToDish();
+            var res = _bo.Create(d);
             return StatusCode(res.Success ? (int)HttpStatusCode.OK : (int)HttpStatusCode.InternalServerError);
         }
 
         [HttpGet("{id}")]
-        public ActionResult<CourseViewModel> Get(Guid id)
+        public ActionResult<DishViewModel> Get(Guid id)
         {
             var res = _bo.Read(id);
             if (res.Success)
             {
                 if (res.Result == null) return NotFound();
-                var cvm = CourseViewModel.Parse(res.Result);
-                return cvm;
+                var dvm = DishViewModel.Parse(res.Result);
+                return dvm;
             }
             else return StatusCode((int)HttpStatusCode.InternalServerError);
         }
 
         [HttpGet]
-        public ActionResult<List<CourseViewModel>> List()
+        public ActionResult<List<DishViewModel>> List()
         {
             var res = _bo.List();
             if (!res.Success) return StatusCode((int)HttpStatusCode.InternalServerError);
-            var list = new List<CourseViewModel>();
+            var list = new List<DishViewModel>();
             foreach (var item in res.Result)
             {
-                list.Add(CourseViewModel.Parse(item));
+                list.Add(DishViewModel.Parse(item));
             }
             return list;
         }
 
 
         [HttpPut]
-        public ActionResult Update([FromBody] CourseViewModel c)
+        public ActionResult Update([FromBody] DishViewModel d)
         {
-            var currentResult = _bo.Read(c.Id);
+            var currentResult = _bo.Read(d.Id);
             if (!currentResult.Success) return StatusCode((int)HttpStatusCode.InternalServerError);
             var current = currentResult.Result;
             if (current == null) return NotFound();
-            if (current.Name == c.Name) return StatusCode((int)HttpStatusCode.NotModified);
+            if (current.Name == d.Name && current.DietaryRestrictionId == d.DietaryRestrictionId) return StatusCode((int)HttpStatusCode.NotModified);
 
 
-            if (current.Name != c.Name) current.Name = c.Name;
+            if (current.Name != d.Name) current.Name = d.Name;
+            if (current.DietaryRestrictionId != d.DietaryRestrictionId) current.DietaryRestrictionId = d.DietaryRestrictionId;
             var updateResult = _bo.Update(current);
             if (!updateResult.Success) return StatusCode((int)HttpStatusCode.InternalServerError);
             return Ok();
